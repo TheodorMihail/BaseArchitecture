37844b4513c74151152c25dce6a12629fc6f4f85
Theodor <Holbantheodormihail@gmail.com>
2026-07-10 17:31:42 +0300

    ParamsExntesions updated with default value return

--- Assets/UnityPackages/BaseArchitecture/Runtime/Core/Tools/ParamsExtensions.cs
+++ Assets/UnityPackages/BaseArchitecture/Runtime/Core/Tools/ParamsExtensions.cs
@@ -2,14 +2,14 @@ namespace BaseArchitecture.Core
 {
     public static class ParamsExtensions
     {
-        public static bool TryGetParam<T>(this object[] paramsList, out T value, int index = 0)
+        public static bool TryGetParam<T>(this object[] paramsList, out T value, T defaultValue = default, int index = 0)
         {
             if (paramsList != null && index < paramsList.Length && paramsList[index] is T param)
             {
                 value = param;
                 return true;
             }
-            value = default;
+            value = defaultValue;
             return false;
         }
     }
